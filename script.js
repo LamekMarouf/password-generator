bb8268459c876cdc43f1066c867ef3a6a61e31d5
Lamek Marouf <lamekmaroufjob@hotmail.com>
2021-12-11 11:06:16 +1100

    Pseudocoding

--- script.js
+++ script.js
@@ -2,39 +2,6 @@
 var generateBtn = document.querySelector("#generate");
 
 
-function generatePassword() {
-  console.log("Button is clicked")
-  var passwordLength = prompt("How long would you like your password?")
-  var passwordLowercase = confirm("Would you like to include lowercase characters?")
-  var passwordUppercase = confirm("Would you like to include uppercase characters?")
-  var passwordNumerical = confirm("Would you like to include numerical characters?")
-  var passwordSpecial = confirm("Would you like to include special characters?")
-
-if (passwordLength < 8 || passwordLength > 128) {
-  alert("Password must be between 8 and 128 charcters")
-  return(" ")
-}
-
-if (passwordLowercase == true) {
-  return("Password should include lowercase characters")
-}
-
-if (passwordUppercase == true) {
-  return("Password should include uppercase characters")
-}
-
-if (passwordNumerical == true) {
-  return("Password should include numerical characters")
-}
-
-if (passwordSpecial == true) {
-  return("Password should include special characters")
-}
-}
-
-
-  
-
 
   // 1. When button is clicked, prompt how long should password be. Between 8 and 128 characters.
   // 2. When length is confirmed, confirm prompt if the user would like: 
